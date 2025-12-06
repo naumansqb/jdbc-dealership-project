9c63ec3a8bbdf4fd94f458cef2a576c78c9e21af
Nauman <naumansqb@gmail.com>
2025-12-06 16:44:04 -0500

    InventoryDao implemented

--- src/main/java/com/yearup/dealership/db/InventoryDao.java
+++ src/main/java/com/yearup/dealership/db/InventoryDao.java
@@ -13,10 +13,32 @@ public class InventoryDao {
     }
 
     public void addVehicleToInventory(String vin, int dealershipId) {
-        // TODO: Implement the logic to add a vehicle to the inventory
+        String query = """
+                INSERT INTO inventory
+                (dealership_id, VIN)
+                VALUES (?, ?)
+                """;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement addStatement = connection.prepareStatement(query)) {
+            addStatement.setInt(1, dealershipId);
+            addStatement.setString(2, vin);
+            addStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeVehicleFromInventory(String vin) {
-        // TODO: Implement the logic to remove a vehicle from the inventory
+        String query = """
+                DELETE FROM inventory
+                WHERE VIN = ?
+                """;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement removeStatement = connection.prepareStatement(query)) {
+            removeStatement.setString(1, vin);
+            removeStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
